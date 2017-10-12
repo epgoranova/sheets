671846ff561a629b05ca59aab785ffdc6d1e7c13
Ekaterina Goranova <epgoranova@gmail.com>
2017-10-12 16:41:20 +0300

    Allow access to the OAuth client config.

--- components/client.go
+++ components/client.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 
 	"golang.org/x/oauth2/google"
 )
@@ -14,12 +15,7 @@ const clientCredentialsPath = "client_credentials.json"
 
 // Client creates a new OAuth2 HTTP client that has the given scope of access.
 func Client(ctx context.Context, scopeURL string) (*http.Client, error) {
-	credentials, err := getClientCredentials()
-	if err != nil {
-		return nil, fmt.Errorf("Unable to read client secret file: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(credentials, scopeURL)
+	config, err := ClientConfig(scopeURL)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to parse client secret file: %v", err)
 	}
@@ -30,6 +26,16 @@ func Client(ctx context.Context, scopeURL string) (*http.Client, error) {
 	}
 
 	return config.Client(ctx, token), nil
+}
+
+// ClientConfig returns an OAuth2 client config for the given scope.
+func ClientConfig(scopeURL string) (*oauth2.Config, error) {
+	credentials, err := getClientCredentials()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read client secret file: %v", err)
+	}
+
+	return google.ConfigFromJSON(credentials, scopeURL)
 }
 
 // getClientCredentials gets OAuth2 client credentials.
